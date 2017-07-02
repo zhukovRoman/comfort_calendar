0ee357a78020f5a52f9f1fe5790259abf2db5849
zhuk99 <zhrovl@gmail.com>
2017-07-02 19:07:23 +0300

    init commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :comfort_calendar do
+  name 'Comfort Calendar plugin'
+  author 'Zhukov R'
+  description 'This is a plugin for PIK-Comfort'
+  version '0.0.1'
+  url 'pik.ru'
+  author_url 'pik.ru'
+end
